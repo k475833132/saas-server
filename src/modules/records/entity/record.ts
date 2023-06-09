55e08ff1f2a02221e16de4770b4ec56012bdad3d
旺仔小馒头 <wangzai@wangzaixiaomantous-MacBook-Pro.local>
2023-06-09 18:52:20 +0800

    records模块

--- src/modules/records/entity/record.ts
+++ src/modules/records/entity/record.ts
@@ -10,8 +10,16 @@ export class DisinfectRecordsEntity extends BaseEntity {
   @Column({ comment: '项目名称', length: 50 })
   projectName: string;
 
-  @Column({ comment: '项目类型', length: 50 })
+  @Column({ comment: '项目类型', length: 50, default: '消杀/清洗' })
   projectType: string;
+
+  // TODO 应该是Date类型，而不是String
+  @Column({ comment: '开始时间' })
+  startingTime: string;
+
+  // TODO 应该是Date类型，而不是String
+  @Column({ comment: '结束时间', default: '消杀/清洗' })
+  endTime: string;
 
   @Column({ comment: '持续时间' })
   duration: number;
